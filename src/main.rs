2f3907aa551bbc660dba8e8a28933741d3557788
Administator <knucker@KnuckerPc.localdomain>
2019-07-01 13:21:09 +0300

    add log

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,25 @@ extern crate percent_encoding;
 use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 
+extern crate log;
+extern crate env_logger;
+
 mod api;
 mod common;
 mod model;
 use common::connection::Database;
+use std::io;
 
-fn main() {
+fn main()-> io::Result<()> {
     let db = Database::new("root", "root", "common", "localhost", 5432);
     let pool = db.connection().expect("Cannot connect to database");
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
     let app = move || {
         App::new()
             .data(pool.clone())
             .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .service(
                 web::resource("/clients")
                     .route(web::get().to_async(api::clients::index))
@@ -32,10 +39,7 @@ fn main() {
                     .route(web::post().to_async(api::clients::update_customer)),
             )
     };
-    dbg!("Constructing the App");
     HttpServer::new(app)
-        .bind("localhost:8088")
-        .expect("cannot_bind")
+        .bind("localhost:8088")?
         .run()
-        .expect("Error running server");
 }
